e4fd49ce4ff0a5630d40fa0254b457ab11c46ecd
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-07-05 12:00:19 +0530

    start

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require('express')
 
 const app = express()
-const path = require('path')
 
+const path = require('path')
 const port = 2882;
+const ejs = require('ejs')
+
 app.use(express.static('public'))
+app.set('view engine', ejs)
 app.listen(port, ()=>{
     console.log("App listening at port " + port);
 })
 
 app.get('/',(req, res)=>{
-    res.json(
-        {
-            name : "Owais Athar"
-        }
-    )
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'about.html'))
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
 })
 
 app.get('/contact', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'contact.html'))
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
+
+app.get('/post', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
 })
 
 // app.get('/about', (req,res)=>{
