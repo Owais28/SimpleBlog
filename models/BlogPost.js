ef0d5aea6a2a446e638c43a50d55095b23e451d0
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-07-05 23:45:13 +0530

    sasd

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 // mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
-const uri = "mongodb+srv://heroku-server-blog:5MrMbZva21tGI49m@learningmongodb.th3y3yh.mongodb.net/my_database?retryWrites=true&w=majority";
-mongoose.connect(uri, {useNewUrlParser: true});
+// const uri = process.env.URI 
+mongoose.connect(process.env.URI || "mongodb+srv://heroku-server-blog:5MrMbZva21tGI49m@learningmongodb.th3y3yh.mongodb.net/my_database?retryWrites=true&w=majority", {useNewUrlParser: true});
 const Schema = mongoose.Schema;
 
 // Blog Post Schema for Blog Post Model
