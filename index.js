ff9ac560aacd3c2a385e272247c1b4a5a66d0b4f
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-07-05 12:20:48 +0530

    update

--- index.js
+++ index.js
@@ -7,25 +7,30 @@ const port = 2882;
 const ejs = require('ejs')
 
 app.use(express.static('public'))
-app.set('view engine', ejs)
+app.set('view engine','ejs')
+
 app.listen(port, ()=>{
     console.log("App listening at port " + port);
 })
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    // res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    // res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact')
 })
 
 app.get('/post', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    // res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
 
 // app.get('/about', (req,res)=>{
